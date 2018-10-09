928d185f0a08559527e200f82801be49b9af062a
germara221 <43453899+germara221@users.noreply.github.com>
2018-10-09 16:00:49 -0400

    Add files via upload
    These are the mean & standard deviation of the first 3 columns of my data.

---
+++ mean & standard deviation.R
@@ -0,0 +1,12 @@
+> mean(planes$V2)
+[1] 2314.775
+> sd(planes$V1, na.rm = TRUE)
+[1] 11.69045
+> sd(planes$V2)
+[1] 877.247
+> sd(planes$V3)
+[1] 137.1426
+> mean(planes$V1)
+[1] 1994.5
+> mean(planes$V3)
+[1] 424.425
